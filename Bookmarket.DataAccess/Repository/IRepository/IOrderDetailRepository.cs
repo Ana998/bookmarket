b674443d40c726f0a1346bd7fc6ef19b0c386fdf
Ana998 <anastasija.gjorgjievska@gmail.com>
2023-05-03 03:37:04 +0200

    Fourth commit

---
+++ Bookmarket.DataAccess/Repository/IRepository/IOrderDetailRepository.cs
@@ -0,0 +1,9 @@
+﻿using Bookmarket.Models;
+
+namespace Bookmarket.DataAccess.Repository.IRepository
+{
+    public interface IOrderDetailRepository : IRepository<OrderDetail>
+    {
+        void Update(OrderDetail obj);
+    }
+}
---
+++ Bookmarket.DataAccess/Repository/OrderDetailRepository.cs
@@ -0,0 +1,19 @@
+﻿using Bookmarket.DataAccess.Repository.IRepository;
+using Bookmarket.Models;
+
+namespace Bookmarket.DataAccess.Repository
+{
+    public class OrderDetailRepository : Repository<OrderDetail>, IOrderDetailRepository
+    {
+        private ApplicationDbContext _db;
+        public OrderDetailRepository(ApplicationDbContext db) : base(db)
+        {
+            _db = db;
+        }
+
+        public void Update(OrderDetail obj)
+        {
+            _db.OrderDetails.Update(obj);
+        }
+    }
+}
